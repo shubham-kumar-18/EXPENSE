006e353ae8b969783b2450f9453db33e75072752
shubham shrivastava <shubhamkumarstm14321@gmail.com>
2026-05-27 18:56:12 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || https://expense-backend-8043.onrender.com
+  baseURL: import.meta.env.VITE_API_URL || "https://expense-backend-8043.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
